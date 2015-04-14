978e034c634cd3a3aa94762f864fd372e8e8a6e7
George White <stonehippo@gmail.com>
2015-04-14 07:42:19 -0400

    A whole lotta changes

---
+++ source/js/raffle.js
@@ -0,0 +1,79 @@
+Parse.initialize("qiAfjSda7bZjurQPX4qnykSSgEfXyrQrvwIQiOZE", "uABcuVxxqvhIX8q0eMwtRyc4mcaLAFXxpJqVUjbH");
+
+var Raffle = (function() {
+  var that = {
+    init: function () {
+      $('#leadForm').submit(that.handleLeadSubmit);
+    },
+    handleLeadSubmit: function(event) {
+      event.preventDefault();
+      that.hideValidationMessages();
+      var lead = that.parseLead(this);
+      if(that.leadIsValid(lead)) {
+        that.saveLead(lead);
+        document.location = "thanks.html";
+      } else {
+        that.invalidateLeadForm(lead);
+      }
+    },
+    hideValidationMessages: function() {
+      $(".validation").fadeOut("fast");
+    },
+    parseLead: function(form) {
+      var raw = $(form).serializeArray(),
+        output = {};
+      $.each(raw, function(index, value) {
+        if (value.name === "interest") {
+          if (!output.interests) {
+            output.interests = [];
+          }
+          output.interests.push(value.value);
+        } else {
+          output[value.name] = $.trim(value.value);
+        }
+      });
+      return output;
+    },
+    invalidateLeadForm: function(lead) {
+
+    },
+    leadIsValid: function(lead) {
+      var isGood = true;
+      var emailRE = /.+@.+\..+/;
+      var invalidHelper = function(key) {
+        if (!lead.invalid) {
+          lead.invalid = [];
+        }
+        if ($.inArray(key, lead.invalid) === -1) {
+          lead.invalid.push(key);
+        }
+        isGood = false;
+      };
+      $.each(lead, function(key, value) {
+        if ($.inArray(key, ["leadName", "emailAddress"]) !== -1) {
+          if (value === "") {
+            invalidHelper(key);
+          }
+          if (key === "emailAddress") {
+            if (!emailRE.test(value)) {
+              console.log(key, value);
+              invalidHelper(key, "");
+            }
+          }
+        }
+      });
+      console.log(lead);
+      return isGood;
+    },
+    saveLead: function(newLead) {
+      var LeadClass = Parse.Object.extend("Lead");
+      var lead = new LeadClass();
+      lead.save(newLead).then(function(object) {
+        // do something with this promise
+      });
+    }
+  };
+  return that;
+})();
+
+Raffle.init();
